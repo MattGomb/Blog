b4d2c231b636bc19154276847b751bd32be393a7
Matyas Gombos <gombosmm@gmail.com>
2023-02-01 13:53:35 -0500

    Add custom methods to models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  # A method that updates the likes counter for a post.
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
+
+  after_save :update_likes_counter
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ApplicationRecord
 
   has_many :comments, dependent: :destroy, foreign_key: :post_id
   has_many :likes, dependent: :destroy, foreign_key: :post_id
+
+  # A method that updates the posts counter for a user.
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  after_save :update_posts_counter
+
+  # A method which returns the 5 most recent comments for a given post.
+  def recent_comments
+    comments.order(created_at: :desc).limit(5).reverse
+  end
 end
