30802e06378ce21af766e254f5babc3c0283e151
Matyas Gombos <gombosmm@gmail.com>
2023-02-01 12:43:13 -0500

    Create models and associations

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+
+  has_many :comments, dependent: :destroy, foreign_key: :post_id
+  has_many :likes, dependent: :destroy, foreign_key: :post_id
+end
