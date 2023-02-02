a1f1f685d679262b046c274a1aab5bb85b03ec0a
Matyas Gombos <gombosmm@gmail.com>
2023-02-02 14:29:58 -0500

    Add tests for like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(name: 'Jack Sparrow', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I\'m Captain Jack Sparrow!', posts_counter: 2)
+    @post = Post.new(author: @user, title: 'The Black Pearl', text: 'What a ship..!', comments_counter: 2, likes_counter: 4)
+  end
+
+  subject { Like.new(author: @user, post: @post) }
+
+  before { subject.save }
+
+  it 'should be a like instance' do
+    expect(subject).to be_an_instance_of(Like)
+  end
+
+  # Tests for associations and connections
+  describe 'the associations of a like' do
+    it 'should belong to an author' do
+      like = Like.reflect_on_association(:author)
+      expect(like.macro).to eq(:belongs_to)
+    end
+
+    it 'should belong to an post' do
+      like = Like.reflect_on_association(:post)
+      expect(like.macro).to eq(:belongs_to)
+    end
+  end
+
+  # Tests for the attributes of like
+  describe 'the attributes of a like' do
+    it 'should have an author' do
+      expect(subject.author).to eq(@user)
+    end
+
+    it 'should have a post' do
+      expect(subject.post).to eq(@post)
+    end
+  end
 end
