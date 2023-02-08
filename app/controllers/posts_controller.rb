e007ab48edf20791532314b2abcf5d87c1285f45
Matyas Gombos <gombosmm@gmail.com>
2023-02-08 16:39:14 -0500

    Create posts_show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,8 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(@post.author_id)
+  end
 end
