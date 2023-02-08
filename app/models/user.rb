412698bd331194349f779e1668af7667c822c092
Matyas Gombos <gombosmm@gmail.com>
2023-02-08 15:12:15 -0500

    Fix validation problem when adding new stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,28 @@ class User < ApplicationRecord
   validates :bio, presence: true, length: { in: 2..500 }
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  
+  
   # A method that returns the 3 most recent posts for a given user.
   def recent_posts
     posts.order(created_at: :desc).limit(3).reverse
   end
-
+  
   # A method that returns the 5 most recent comments for a given user.
   def recent_comments
     comments.order(created_at: :desc).limit(5).reverse
   end
-
+  
   # A method that returns the 5 most recent likes for a given user.
   def recent_likes
     likes.order(created_at: :desc).limit(5).reverse
+  end
+  
+  before_validation :set_defaults
+
+  private
+
+  def set_defaults
+    self.posts_counter = 0
   end
 end
