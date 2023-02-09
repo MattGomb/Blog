1c19ce7f766f33a1e2efc22343f0488a1f2db739
Matyas Gombos <gombosmm@gmail.com>
2023-02-08 19:02:50 -0500

    Reset root route to /users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ('/')
-  # root 'articles#index'
+  root 'users#index'
   
   # Route to list all users
   # Route to show a specific user
